234739000ea78d5933b63b6d09f9ce4a6383c606
nimrod libman <nimrodlibman@yahoo.c.uk>
2014-03-23 07:43:32 +0000

    First commit of the branch

---
+++ visualrun/visualrun/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace visualrun
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Show the dialog and get result.
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK) // Test result.
+            {
+ 
+            }
+            Console.WriteLine(result); // <-- For debugging use.
+        }
+    }
+}
