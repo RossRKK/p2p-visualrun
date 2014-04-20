f9f444cd75f8ddb6056186e39c0f8b688ae49abd
nimrod libman <nimrodlibman@yahoo.c.uk>
2014-04-20 18:57:40 +0100

    Prep for later

--- visualrun/visualrun/Form1.cs
+++ visualrun/visualrun/Form1.cs
@@ -6,11 +6,13 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
-
+using System.Diagnostics;
+using System.IO;
 namespace visualrun
 {
     public partial class Form1 : Form
     {
+        private string file;
         public Form1()
         {
             string path =System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase);
@@ -19,16 +21,30 @@ namespace visualrun
             this.openFileDialog1.FileName = "";
             openFileDialog1.InitialDirectory = path;
         }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
+        private void FileAway() {
             // Show the dialog and get result.
             DialogResult result = openFileDialog1.ShowDialog();
             
             if (result == DialogResult.OK) // Test result.
             {
+
+                File.WriteAllText("file.txt", openFileDialog1.FileName);
             }
-           Console.Write(result); // <-- For debugging use.
+           Console.Write(result); // <-- For debugging use. 
+        }
+        private void Run() {
+            file = File.ReadAllText("file.txt");
+            Process.Start(file);
+        
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FileAway();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Run();
         }
     }
 }
